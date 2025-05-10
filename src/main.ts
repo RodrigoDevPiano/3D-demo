11b40dd8e61899a87a93c47dd6c91a91451493ac
Rodrigo <rolfi46@hotmail.com>
2025-05-09 18:32:38 -0600

    delete model duplicate

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,4 @@ const viewer = document.createElement("model-viewer");
 viewer.setAttribute("src", "unidad1.glb");
 viewer.setAttribute("auto-rotate", "");
 viewer.setAttribute("camera-controls", "");
-document.body.appendChild(viewer);
+// document.body.appendChild(viewer);
